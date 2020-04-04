e5311884ebeeb39c7f87aa8b0012a13dde664d64
Sam Davis <sam@sjd.co>
2020-04-04 13:25:01 +1100

    Attempt sharp fix

--- scripts/dependancy-installer.js
+++ scripts/dependancy-installer.js
@@ -4,7 +4,15 @@ const globby = require("globby");
 const cp = require("child_process");
 
 function installDeps(functionDir, cb) {
-    cp.exec("npm i", { cwd: functionDir }, cb);
+    cp.exec("npm i", { cwd: functionDir }, () => {
+        cp.exec("rm -rf node_modules/sharp", { cwd: functionDir }, () => {
+            cp.exec(
+                "npm install --arch=x64 --platform=linux sharp",
+                { cwd: functionDir },
+                cb
+            );
+        });
+    });
 }
 
 (async () => {
@@ -13,10 +21,10 @@ function installDeps(functionDir, cb) {
     const foldersWithDeps = await globby(findJSFiles, { cwd: directory });
 
     const folders = foldersWithDeps
-        .map(fnFolder => {
+        .map((fnFolder) => {
             return fnFolder.substring(0, fnFolder.indexOf("package.json"));
         })
-        .map(folder => {
+        .map((folder) => {
             installDeps(path.join(__dirname, "..", "functions", folder), () => {
                 console.log(`${folder} dependencies installed`);
             });
