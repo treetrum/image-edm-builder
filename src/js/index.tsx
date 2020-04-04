d6a4b9ab841de508a42aeb33966c7ef6213a3a4f
Sam Davis <sam@sjd.co>
2020-04-04 17:04:49 +1100

    Add auth

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,9 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as NetlifyIdentityWidget from "netlify-identity-widget";
 
 import ImageAdder from "./components/ImageAdder";
 
+NetlifyIdentityWidget.init();
+
 const App = () => {
+    const [user, setUser] = React.useState(NetlifyIdentityWidget.currentUser());
+    React.useEffect(() => {
+        NetlifyIdentityWidget.open("login");
+        NetlifyIdentityWidget.on("login", (user) => {
+            setUser(user);
+        });
+    }, []);
+    if (!user) {
+        return null;
+    }
     return <ImageAdder />;
 };
 
